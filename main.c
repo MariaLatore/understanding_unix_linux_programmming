cde2f923aef38c5238e0ce50c2e6953d4922047f
zxy <870508213@qq.com>
2026-07-28 14:44:29 +0800

    little fix
    1. Try to fix the flash phenomenean using system internal timer, but still fail.
    2. Main failure: when press move keys, the speed of the move is not the same.

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <curses.h>
 #include <signal.h>
+#include <sys/time.h>
 
 #define DFL_SYMBOL 'o'
 #define BLANK  ' '
@@ -14,6 +15,7 @@ typedef struct {
 
 ppball_t ball = {0};
 int done = 0;
+int tick = 0;
 
 void move_ball(int newxdir, int newydir){
 	mvaddch(ball.y_pos, ball.x_pos, BLANK);
@@ -29,26 +31,18 @@ void move_ball(int newxdir, int newydir){
 	mvaddch(ball.y_pos, ball.x_pos, ball.symbol);
 	refresh();
 }
+
+void on_tick(int sig){
+	tick = 1;
+}
 	
 
-int main(){
-	//init curse
-	initscr();
-	cbreak();
-	noecho();
-	clear();
-	refresh();
-        curs_set(0); 
-	signal(SIGINT, SIG_IGN);
-
-	ball.symbol = DFL_SYMBOL;
-	ball.x_pos = COLS/2;
-	ball.y_pos = LINES/2;
-	
-	char c;
-	move_ball(0,0);
-	while((c = getch())){
-		switch(c){
+void on_input(){
+	int c=ERR,now;
+	while((now = getch())!=ERR){
+		c = now;
+	}
+	switch(c){
 			case 'q':
 				done=1;break;
 			case 'w':
@@ -61,10 +55,65 @@ int main(){
 				move_ball(1,0); break;
 			default:
 				break;
-		}
-		if(done) break;
 	}
+	return ;
 
+}
+
+int setup_ticker(int n_msecs){
+	struct itimerval new_timeset;
+	long n_sec, n_usecs;
+
+	n_sec = n_msecs/1000;
+	n_usecs = (n_msecs%1000)*1000;
+
+	new_timeset.it_interval.tv_sec = n_sec;
+	new_timeset.it_interval.tv_usec = n_usecs;
+	new_timeset.it_value.tv_sec = n_sec;
+	new_timeset.it_value.tv_usec = n_usecs;
+	return setitimer(ITIMER_REAL, &new_timeset, NULL);
+}
+
+void init(void){
+	//init curse
+	initscr();
+	cbreak();
+	noecho();
+	clear();
+	refresh();
+        curs_set(0);  //do not display cursor 
+	nodelay(stdscr, TRUE);  //getch unblock
+	
+	//signal init
+	signal(SIGINT, SIG_IGN);
+	signal(SIGALRM, on_tick);
+
+	//ball pos init
+	ball.symbol = DFL_SYMBOL;
+	ball.x_pos = COLS/2;
+	ball.y_pos = LINES/2;
+	
+	move_ball(0,0);
+
+	//init ticker
+	setup_ticker(10);
+}
+
+int main(){
+	sigset_t block_sigs;
+	sigset_t old_sigs;
+	sigemptyset(&block_sigs);
+	sigaddset(&block_sigs, SIGALRM);
+	init();
+	while(done == 0){
+		if(tick == 1){
+			sigprocmask(SIG_BLOCK, &block_sigs, &old_sigs);
+			tick=0;
+			on_input();
+			sigprocmask(SIG_SETMASK, &old_sigs, NULL);
+		}
+	}
+	
 	endwin();
 	return 0;
 
